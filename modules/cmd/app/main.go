cdb3ec34b82bea8cd709ba74ada85c95778a6f21
Dimon <glinozem@yahoo.com>
2026-01-25 18:50:39 +0300

    lesson-2: add fatih/color, tidy deps, ignore IDE/build artifacts

--- modules/cmd/app/main.go
+++ modules/cmd/app/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/google/uuid"
+	"github.com/fatih/color"
 	"github.com/glinozem/golang-scl-less-2/modules/internal/usecase"
+	"github.com/google/uuid"
 	_ "go.uber.org/automaxprocs"
 )
 
 func main() {
 	hello := usecase.NewHello()
-	fmt.Println(hello.Say())
+	color.Green(hello.Say())
+	color.Cyan("UUID: %s", uuid.New().String())
 
-	fmt.Println("UUID:", uuid.New().String())
+	//fmt.Println("UUID:", uuid.New().String())
 }
